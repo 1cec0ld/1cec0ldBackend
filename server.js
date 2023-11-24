6fed0d4879a7a399ea36c50f05f2fb04c3cc1a41
1cec0ld <1cew1r3@gmail.com>
2023-11-24 14:51:45 -0800

    20231124 Initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const fs = require ('fs');
+const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const config = {
+  name: 'ereq-server',
+  port: 3000,
+  host: '0.0.0.0',
+};
+const certificates = {
+  cert:fs.readFileSync('./dev_rxight_web_certs/localhost.cer'), 
+  ca:fs.readFileSync('./dev_rxight_web_certs/ca.crt'), 
+  key:fs.readFileSync('./dev_rxight_web_certs/localhost.key')
+}
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.status(200).send('hello world');
+  console.log("get: ",200);
+  console.log(process.env)
+});
+
+const httpsServer = https.createServer(certificates, app);
+httpsServer.listen(config.port, config.host, (err)=> {
+  if(err) {
+    throw new Error('Internal Server Error');
+  }
+});
