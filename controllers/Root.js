30d0a9091d85089ab2f90fd536d4faa9f9f147f6
Jeremy Luchak <1cew1r3@gmail.com>
2023-12-26 22:06:31 -0800

    20231226 Stop showing ENV you gaffer

--- controllers/Root.js
+++ controllers/Root.js
@@ -7,7 +7,7 @@ const Root = (request, response) => {
     console.log('LoadBalancer HealthCheck '+ new Date().toISOString().replace('T',' ') )
     return response.status(200).send('');
   }
-  response.status(200).send(JSONPretty(process.env));
+  response.status(200).send(JSONPretty(process.env.GIT_REV));
 }
 
 export default Root
